83dd25b3b1e6969a0127f7eab8b456588d22006d
Emre Mert Akdağ <emremakdag55@gmail.com>
2023-02-10 03:39:44 +0300

    Initial commit

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Login.scss';
+
+type Props = {};
+
+const Login = (props: Props) => {
+  return (
+    <div className='container'>
+      <div className='card'>
+        <p>Connect Your Wallet</p>
+        <button>BAĞLA</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
